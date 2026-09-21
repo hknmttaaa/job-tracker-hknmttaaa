e4cec2db03c6b16599fac19130a50fbe999193cd
hknmttaaa <restyhfm14@gmail.com>
2026-09-21 16:36:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -210,7 +210,7 @@ with st.sidebar.form("add_form", clear_on_submit=True):
 # --- DASHBOARD UTAMA ---
 if not df.empty and "Hasil" in df.columns:
     if "active_view" not in st.session_state:
-    st.session_state.active_view = "ALL"
+        st.session_state.active_view = "ALL"
 
 # Cek interaksi dari klik kartu HTML
 query_params = st.query_params
